4121248048d1cf7289264c3518be5d1b0a959544
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-10-06 14:34:35 +0300

    removed unrequited lombok annotations

--- src/main/java/tdr/task/botscrewtask/model/dto/DepartmentDto.java
+++ src/main/java/tdr/task/botscrewtask/model/dto/DepartmentDto.java
@@ -1,7 +1,5 @@
 package tdr.task.botscrewtask.model.dto;
 
-import lombok.EqualsAndHashCode;
-import lombok.Value;
 import tdr.task.botscrewtask.model.entity.Lector;
 
 import java.io.Serializable;
@@ -10,8 +8,6 @@ import java.util.Set;
 /**
  * DTO for {@link tdr.task.botscrewtask.model.entity.Department}
  */
-@Value
-@EqualsAndHashCode(callSuper = true)
 public record DepartmentDto(Long id, String name, String description, String location,
                             Set<Lector> lectors, LectorDto head) implements Serializable {
 }
