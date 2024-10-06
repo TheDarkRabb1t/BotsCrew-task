1ef8739a3c2fe8fd32f8a47601b60d7b76b8c6a3
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-10-06 16:27:10 +0300

    added lector repository methods

--- src/main/java/tdr/task/botscrewtask/repository/LectorRepository.java
+++ src/main/java/tdr/task/botscrewtask/repository/LectorRepository.java
@@ -1,7 +1,15 @@
 package tdr.task.botscrewtask.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tdr.task.botscrewtask.model.entity.Lector;
 
 public interface LectorRepository extends JpaRepository<Lector, Long> {
+    @Query(value = """
+            select *
+            from lector l
+            where l.full_name ILIKE concat('%', :value, '%');
+            """, nativeQuery = true)
+    Integer globalSearchByValue(@Param("value") String value);
 }
