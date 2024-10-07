1239d18e670aa84f90f2f1bbd17f9d15c4900c0d
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-10-07 16:16:16 +0300

    fixed repositories

--- src/main/java/tdr/task/botscrewtask/repository/DepartmentRepository.java
+++ src/main/java/tdr/task/botscrewtask/repository/DepartmentRepository.java
@@ -3,10 +3,12 @@ package tdr.task.botscrewtask.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import tdr.task.botscrewtask.model.entity.Department;
 
 import java.util.List;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department getDepartmentByName(String name);
 
--- src/main/java/tdr/task/botscrewtask/repository/LectorRepository.java
+++ src/main/java/tdr/task/botscrewtask/repository/LectorRepository.java
@@ -3,10 +3,12 @@ package tdr.task.botscrewtask.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import tdr.task.botscrewtask.model.entity.Lector;
 
 import java.util.List;
 
+@Repository
 public interface LectorRepository extends JpaRepository<Lector, Long> {
     @Query(value = """
             select *
