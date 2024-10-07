b44420b2d3c2e0240861ca331fe631aec127d3de
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-10-07 16:17:07 +0300

    made application bootable as console runner

--- src/main/java/tdr/task/botscrewtask/BotsCrewTaskApplication.java
+++ src/main/java/tdr/task/botscrewtask/BotsCrewTaskApplication.java
@@ -1,13 +1,46 @@
 package tdr.task.botscrewtask;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tdr.task.botscrewtask.command.CommandProcessor;
+import tdr.task.botscrewtask.command.CommandResult;
+import tdr.task.botscrewtask.exception.AmbiguousCommandException;
+import tdr.task.botscrewtask.exception.CommandNotRecognisedException;
+
+import java.util.Scanner;
 
 @SpringBootApplication
-public class BotsCrewTaskApplication {
+public class BotsCrewTaskApplication implements CommandLineRunner {
+
+    private final CommandProcessor commandProcessor;
+
+    public BotsCrewTaskApplication(CommandProcessor commandProcessor) {
+        this.commandProcessor = commandProcessor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BotsCrewTaskApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter commands (type 'exit' to quit):");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if ("exit".equalsIgnoreCase(input)) {
+                break;
+            }
+
+            try {
+                CommandResult result = commandProcessor.process(input);
+                System.out.println(result.asString());
+            } catch (CommandNotRecognisedException | AmbiguousCommandException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        scanner.close();
+    }
 }
